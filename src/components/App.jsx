faa31c387c2196a785d3c40d93a840d14e6e55fb
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2024-01-08 23:16:28 +0200

    add FeedbackOptions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,31 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const currentKey = e.target.textContent.toLowerCase();
+    if (!this.state.hasOwnProperty(currentKey)) return;
+    this.setState(prevState => {
+      return { [currentKey]: prevState[currentKey] + 1 };
+    });
+  };
+
   render() {
-    return <Section title="Please leave feedback"></Section>;
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+    );
   }
 }
 export { App };
