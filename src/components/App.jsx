65f510290293ae4916240d115918a2a2bf4dc404
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2024-01-11 22:54:04 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
 class App extends Component {
   state = {
@@ -32,15 +33,22 @@ class App extends Component {
           options={Object.keys(this.state)}
           onLeaveFeedback={this.onLeaveFeedback}
         />
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={totalFeedbacks}
-          positivePercentage={this.countPositiveFeedbackPercentage(
-            totalFeedbacks
+        <div>
+          <h2>Statistics</h2>
+          {totalFeedbacks ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={totalFeedbacks}
+              positivePercentage={this.countPositiveFeedbackPercentage(
+                totalFeedbacks
+              )}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
           )}
-        />
+        </div>
       </Section>
     );
   }
